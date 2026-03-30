25eeaa9cf192ced83afe4a2deedc0e6514e518f4
justinlocke1210 <justin.locke1210@gmail.com>
2026-03-30 14:15:44 -0600

    Add TypeScript types for referenced file API

---
+++ src/types/electron.d.ts
@@ -0,0 +1,12 @@
+export {};
+
+declare global {
+  interface Window {
+    desktopAPI?: {
+      selectDesignFiles: (designId: string) => Promise<any[]>;
+      openDesignFile: (filePath: string) => Promise<{ ok: true }>;
+      removeDesignFileReference: (designId: string, fileId: string) => Promise<{ ok: boolean }>;
+      listDesignFileReferences: (designId: string) => Promise<any[]>;
+    };
+  }
+}
