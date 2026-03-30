8ac6dae5687f608bef74e31845f590db2f8da1a0
justinlocke1210 <justin.locke1210@gmail.com>
2026-03-30 15:20:22 -0600

    title: "Design Threader"

--- electron/main.cjs
+++ electron/main.cjs
@@ -41,6 +41,7 @@ function createWindow() {
     height: 800,
     minWidth: 1000,
     minHeight: 700,
+    title: "Design Threader",
     webPreferences: {
       preload: path.join(__dirname, "preload.cjs"),
       contextIsolation: true,
