b32f4cc83fcd1fdf1a2e6dcae306d7f21a931da0
justinlocke1210 <justin.locke1210@gmail.com>
2026-03-30 13:43:17 -0600

    Remove Electron debug tools

--- electron/main.cjs
+++ electron/main.cjs
@@ -5,6 +5,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    minWidth: 1000,
+    minHeight: 700,
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false
@@ -12,19 +14,7 @@ function createWindow() {
   });
 
   const indexPath = path.join(__dirname, "..", "dist", "index.html");
-  console.log("Loading file:", indexPath);
-
   win.loadFile(indexPath);
-
-  win.webContents.openDevTools();
-
-  win.webContents.on("did-fail-load", (_event, errorCode, errorDescription, validatedURL) => {
-    console.log("did-fail-load:", { errorCode, errorDescription, validatedURL });
-  });
-
-  win.webContents.on("console-message", (_event, level, message, line, sourceId) => {
-    console.log("renderer console:", { level, message, line, sourceId });
-  });
 }
 
 app.whenReady().then(createWindow);
