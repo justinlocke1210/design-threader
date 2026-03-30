8cb32617810652319e24a39c2d3b5529f8b6659d
justinlocke1210 <justin.locke1210@gmail.com>
2026-03-30 12:52:19 -0600

    Add Electron diagnostics

--- electron/main.cjs
+++ electron/main.cjs
@@ -5,9 +5,30 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
   });
 
-  win.loadFile(path.join(__dirname, "..", "dist", "index.html"));
+  const indexPath = path.join(__dirname, "..", "dist", "index.html");
+  console.log("Loading file:", indexPath);
+
+  win.loadFile(indexPath);
+
+  win.webContents.openDevTools();
+
+  win.webContents.on("did-fail-load", (_event, errorCode, errorDescription, validatedURL) => {
+    console.log("did-fail-load:", { errorCode, errorDescription, validatedURL });
+  });
+
+  win.webContents.on("console-message", (_event, level, message, line, sourceId) => {
+    console.log("renderer console:", { level, message, line, sourceId });
+  });
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
