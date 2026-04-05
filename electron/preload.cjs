a83260825536ba5d404f67fdfe909d9e38ad2377
justinlocke1210 <justin.locke1210@gmail.com>
2026-04-04 18:57:41 -0600

    Expose app version from Electron preload

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,6 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
+const pkg = require("../package.json");
 
 contextBridge.exposeInMainWorld("desktopAPI", {
+  version: pkg.version,
   selectDesignFiles: (designId) => ipcRenderer.invoke("design-files:select", designId),
   openDesignFile: (filePath) => ipcRenderer.invoke("design-files:open", filePath),
   removeDesignFileReference: (designId, fileId) => ipcRenderer.invoke("design-files:remove-reference", designId, fileId),
